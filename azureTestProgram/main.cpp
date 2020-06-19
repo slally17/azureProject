950ee52c04a5583c3bf7a6cee5d31e75a2aa09b2
Sam Lally <slally17@vt.edu>
2020-06-19 18:57:43 -0400

    First version of the project

---
+++ azureTestProgram/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include <k4a/k4a.h>
+#include <k4arecord/record.h>
+#include <k4arecord/playback.h>
+
+#include <fbxsdk.h>
+
+int main(int argc, char **argv) 
+{
+	std::cout << "test" << std::endl;
+	return 0;
+}
