39d95200a0dc4f9555a05589e81bed6b3d51f80d
Sam Lally <slally17@vt.edu>
2020-07-10 16:40:46 -0400

    Included osc files into main.

--- azureTestProgram/main.cpp
+++ azureTestProgram/main.cpp
@@ -10,6 +10,13 @@
 
 #include <fbxsdk.h>
 
+#include "oscpack/osc/OscOutboundPacketStream.h"
+#include "oscpack/ip/UdpSocket.h"
+
+#define ADDRESS "127.0.0.1"
+#define PORT 7000
+#define OUTPUT_BUFFER_SIZE 1024
+
 #include "modeOneFunctions.h"
 #include "modeTwoFunctions.h"
 #include "modeThreeFunctions.h"
