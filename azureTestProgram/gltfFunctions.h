02af7974ae45863e03c106ee54a5c4de1a5307d4
Sam Lally <slally17@vt.edu>
2020-07-23 14:11:28 -0400

    gltf exporter created.

--- azureTestProgram/gltfFunctions.h
+++ azureTestProgram/gltfFunctions.h
@@ -9,6 +9,7 @@
 
 #include <experimental/filesystem>
 #include <fstream>
+#include <vector>
 
 using namespace Microsoft::glTF;
 
@@ -49,117 +50,82 @@ namespace {
 		std::experimental::filesystem::path m_pathBase;
 	};
 
-	void CreateTriangleResources(Document& document, BufferBuilder& bufferBuilder, std::string& accessorIdIndices, std::string& accessorIdPositions)
-	{
-
-		// Create all the resource data (e.g. triangle indices and
-		// vertex positions) that will be written to the binary buffer
-		const char* bufferId = nullptr;
-
-		// Specify the 'special' GLB buffer ID. This informs the GLBResourceWriter that it should use
-		// the GLB container's binary chunk (usually the desired buffer location when creating GLBs)
-		if (dynamic_cast<const GLBResourceWriter*>(&bufferBuilder.GetResourceWriter()))
-		{
-			bufferId = GLB_BUFFER_ID;
+	void addPositionData(std::vector<k4abt_skeleton_t> skeletons, std::vector<float> positions[27]) {
+		for (int i = 0; i < 27; i++) {	
+			for (int j = 0; j < skeletons.size(); j++) {
+				positions[i].push_back(skeletons[j].joints[i].position.xyz.x);
+				positions[i].push_back(skeletons[j].joints[i].position.xyz.y);
+				positions[i].push_back(skeletons[j].joints[i].position.xyz.z);
+			}
 		}
+	}
 
-		// Create a Buffer - it will be the 'current' Buffer that all the BufferViews
-		// created by this BufferBuilder will automatically reference
+	void CreateSkeletonResources(std::vector<k4abt_skeleton_t> skeletons, Document& document, BufferBuilder& bufferBuilder, std::string& accessorIdTime, std::string accessorIdPositions[27]) {
+		//Create buffer to store all resource data
+		const char* bufferId = nullptr;
 		bufferBuilder.AddBuffer(bufferId);
 
-		// Create a BufferView with a target of ELEMENT_ARRAY_BUFFER (as it will reference index
-		// data) - it will be the 'current' BufferView that all the Accessors created by this
-		// BufferBuilder will automatically reference
+		//Create buffer view for keyframe times
 		bufferBuilder.AddBufferView(BufferViewTarget::ELEMENT_ARRAY_BUFFER);
-		
-		// Add an Accessor for the indices
-		std::vector<uint16_t> indices = {
-			0U, 1U, 2U
-		};
+		std::vector<float> times; //Create times based off input being 30fps
+		for (int i = 0; i < skeletons.size(); i++) {
+			times.push_back(i / 30.0f);
+		}
+		accessorIdTime = bufferBuilder.AddAccessor(times, { TYPE_SCALAR, COMPONENT_FLOAT }).id;
 
-		// Copy the Accessor's id - subsequent calls to AddAccessor may invalidate the returned reference
-		accessorIdIndices = bufferBuilder.AddAccessor(indices, { TYPE_SCALAR, COMPONENT_UNSIGNED_SHORT }).id; //THIS LINE BROKEN
-
-		// Create a BufferView with target ARRAY_BUFFER (as it will reference vertex attribute data)
+		//Create buffer views for animation node data
 		bufferBuilder.AddBufferView(BufferViewTarget::ARRAY_BUFFER);
-
-		// Add an Accessor for the positions
-		std::vector<float> positions = {
-			0.0f, 0.0f, 0.0f, // Vertex 0
-			1.0f, 0.0f, 0.0f, // Vertex 1
-			0.0f, 1.0f, 0.0f  // Vertex 2
-		};
-
-		std::vector<float> minValues(3U, std::numeric_limits<float>::max());
-		std::vector<float> maxValues(3U, std::numeric_limits<float>::lowest());
-
-		const size_t positionCount = positions.size();
-
-		// Accessor min/max properties must be set for vertex position data so calculate them here
-		for (size_t i = 0U, j = 0U; i < positionCount; ++i, j = (i % 3U))
-		{
-			minValues[j] = std::min(positions[i], minValues[j]);
-			maxValues[j] = std::max(positions[i], maxValues[j]);
+		std::vector<float> positions[27] = {};
+		addPositionData(skeletons, positions);
+		std::vector<float> minValues[27]; 
+		std::vector<float> maxValues[27]; 
+		for (int i = 0; i < 27; i++) {
+			minValues[i] = std::vector<float>(3U, std::numeric_limits<float>::max());
+			maxValues[i] = std::vector<float>(3U, std::numeric_limits<float>::lowest());
+		}
+		const size_t positionCount = positions[0].size();
+		for (int h = 0; h < 27; h++) {
+			for (size_t i = 0U, j = 0U; i < positionCount; ++i, j = (i % 3U)) {
+				minValues[h][j] = std::min(positions[h][i], minValues[h][j]);
+				maxValues[h][j] = std::max(positions[h][i], maxValues[h][j]);
+			}
+		}		
+		for (int i = 0; i < 27; i++) {
+			accessorIdPositions[i] = bufferBuilder.AddAccessor(positions[i], { TYPE_VEC3, COMPONENT_FLOAT, false, std::move(minValues[i]), std::move(maxValues[i]) }).id;
 		}
 
-		accessorIdPositions = bufferBuilder.AddAccessor(positions, { TYPE_VEC3, COMPONENT_FLOAT, false, std::move(minValues), std::move(maxValues) }).id;
-
-		// Add all of the Buffers, BufferViews and Accessors that were created using BufferBuilder to
-		// the Document. Note that after this point, no further calls should be made to BufferBuilder
+		//Add everything created above into the document
 		bufferBuilder.Output(document);
 	}
 
-	void CreateTriangleEntities(Document& document, const std::string& accessorIdIndices, const std::string& accessorIdPositions)
-	{
-		// Create a very simple glTF Document with the following hierarchy:
-		//  Scene
-		//     Node
-		//       Mesh (Triangle)
-		//         MeshPrimitive
-		//           Material (Blue)
-		// 
-		// A Document can be constructed top-down or bottom up. However, if constructed top-down
-		// then the IDs of child entities must be known in advance, which prevents using the glTF
-		// SDK's automatic ID generation functionality.
+	void CreateSkeletonEntities(Document& document, const std::string& accessorIdTime, const std::string accessorIdPositions[27]) {	
+		Node skeleton[27];
+		std::string skeletonId[27];
+		for (int i = 0; i < 27; i++) {
+			skeletonId[i] = document.nodes.Append(std::move(skeleton[i]), AppendIdPolicy::GenerateOnEmpty).id;
+		}
 
-		// Construct a Material
-		Material material;
-		material.metallicRoughness.baseColorFactor = Color4(0.0f, 0.0f, 1.0f, 1.0f);
-		material.metallicRoughness.metallicFactor = 0.2f;
-		material.metallicRoughness.roughnessFactor = 0.4f;
-		material.doubleSided = true;
+		AnimationSampler skeletonAnimationSampler[27];
+		AnimationTarget skeletonAnimationTarget[27];
+		AnimationChannel skeletonAnimationChannel[27];
+		Animation skeletonAnimation[27];
+		for (int i = 0; i > 27; i++) {
+			skeletonAnimationSampler[i].inputAccessorId = accessorIdTime;
+			skeletonAnimationSampler[i].outputAccessorId = accessorIdPositions[i];
+			skeletonAnimationTarget[i].nodeId = skeletonId[i];
+			skeletonAnimationTarget[i].path = TARGET_TRANSLATION;
+			skeletonAnimationChannel[i].samplerId = skeletonAnimationSampler[i].id;
+			skeletonAnimationChannel[i].target = skeletonAnimationTarget[i];
+			skeletonAnimation[i].samplers.Append(std::move(skeletonAnimationSampler[i]), AppendIdPolicy::GenerateOnEmpty);
+			skeletonAnimation[i].channels.Append(std::move(skeletonAnimationChannel[i]), AppendIdPolicy::GenerateOnEmpty);
+		}
 
-		// Add it to the Document and store the generated ID
-		auto materialId = document.materials.Append(std::move(material), AppendIdPolicy::GenerateOnEmpty).id;
-
-		// Construct a MeshPrimitive. Unlike most types in glTF, MeshPrimitives are direct children
-		// of their parent Mesh entity rather than being children of the Document. This is why they
-		// don't have an ID member.
-		MeshPrimitive meshPrimitive;
-		meshPrimitive.materialId = materialId;
-		meshPrimitive.indicesAccessorId = accessorIdIndices;
-		meshPrimitive.attributes[ACCESSOR_POSITION] = accessorIdPositions;
-
-		// Construct a Mesh and add the MeshPrimitive as a child
-		Mesh mesh;
-		mesh.primitives.push_back(std::move(meshPrimitive));
-		// Add it to the Document and store the generated ID
-		auto meshId = document.meshes.Append(std::move(mesh), AppendIdPolicy::GenerateOnEmpty).id;
-
-		// Construct a Node adding a reference to the Mesh
-		Node node;
-		node.meshId = meshId;
-		// Add it to the Document and store the generated ID
-		auto nodeId = document.nodes.Append(std::move(node), AppendIdPolicy::GenerateOnEmpty).id;
-
-		// Construct a Scene
 		Scene scene;
-		scene.nodes.push_back(nodeId);
-		// Add it to the Document, using a utility method that also sets the Scene as the Document's default
+		for (int i = 0; i < 27; i++) {
+			scene.nodes.push_back(skeletonId[i]);
+		}		
 		document.SetDefaultScene(std::move(scene), AppendIdPolicy::GenerateOnEmpty);
-	}
-
-
+	}	
 
 	bool createGLTF(std::vector<k4abt_skeleton_t> skeletons, const char* output_path) {
 		bool result = true;
@@ -183,13 +149,13 @@ namespace {
 
 		//Create gltf JSON manifest
 		Document document;
-		std::string accessorIdIndices;
-		std::string accessorIdPositions;
+		std::string accessorIdTime;
+		std::string accessorIdPositions[27];
 		BufferBuilder bufferBuilder(std::move(resourceWriter));
 
 		//Create gltf assets
-		CreateTriangleResources(document, bufferBuilder, accessorIdIndices, accessorIdPositions);
-		CreateTriangleEntities(document, accessorIdIndices, accessorIdPositions);
+		CreateSkeletonResources(skeletons, document, bufferBuilder, accessorIdTime, accessorIdPositions);
+		CreateSkeletonEntities(document, accessorIdTime, accessorIdPositions);
 
 		// Serialize the glTF Document into a JSON manifest
 		std::string manifest;
@@ -203,8 +169,12 @@ namespace {
 
 		//Write the JSON manifest to file
 		auto& gltfResourceWriter = bufferBuilder.GetResourceWriter();
-		gltfResourceWriter.WriteExternal(pathFile.u8string(), manifest.c_str(), manifest.length()); //THIS LINE BROKEN
+		gltfResourceWriter.WriteExternal(pathFile.u8string(), manifest.c_str(), manifest.length());
 		
+		if (result) {
+			std::cout << "Program Success!\n";
+		}
+
 		return result;
 	}
 }
